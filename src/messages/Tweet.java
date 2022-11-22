6de8cd49ecf74cc0113fa6f83687082a1af5f587
dtchitt <danielchittenden@yahoo.com>
2022-11-22 09:59:37 -0800

    update tweet

--- src/messages/Tweet.java
+++ src/messages/Tweet.java
@@ -1,5 +1,17 @@
 package messages;
 
 public class Tweet implements IMessage {
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String display() {
+		return null;
+		// TODO Auto-generated method stub
+	}
 	
 }
